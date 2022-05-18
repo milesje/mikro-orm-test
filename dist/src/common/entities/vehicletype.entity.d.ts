68b7ff91f4b6706218aa14eb89c7cd9bebff6a87
Jacob Miles <jacob.e.miles@pm.me>
2022-05-18 08:27:06 -0500

    removed output and node_modules

--- dist/src/common/entities/vehicletype.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { Collection } from '@mikro-orm/core';
-import { Vehicle } from './vehicle.entity';
-export declare class VehicleType {
-    id: string;
-    description: string;
-    unitType: string;
-    vehicles: Collection<Vehicle, unknown>;
-}
